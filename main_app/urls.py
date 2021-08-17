1d99f53e8461fed400552fe6f7a8dc294f3d438c
Emmanuel John J. Bonifacio <76241888+ejjbonifacio@users.noreply.github.com>
2021-08-17 23:42:02 +0800

    feat: Login page/view (user registration is yet to be functional)

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
+    path('register', views.register, name='register'),
 ]
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from .models import User
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -7,4 +12,56 @@ def index(request):
         return render(request, "main_app/splash.html")
     else:
         return render(request, "main_app/index.html")
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "main_app/login.html")
+    else:
+        # POST method
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username,
+            password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "main_app/login.html", {
+                "message": "Invalid username and/or password."
+            })
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+ 
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        nickname = request.POST["nickname"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "main_app/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "main_app/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "main_app/register.html")
+
 
