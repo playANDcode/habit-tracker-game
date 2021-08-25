2fa8488ec0fcc9912a5eb9870fc9bfafad2d0877
Emmanuel John J. Bonifacio <76241888+ejjbonifacio@users.noreply.github.com>
2021-08-25 19:03:39 +0800

    feat: Template for the rewards

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     nickname = models.CharField(max_length=16)
     todo = models.ManyToManyField("Todo")
+    avatar = models.CharField(max_length=16)
+    health_current = models.IntegerField()
+    health_max = models.IntegerField()
+    exp_current = models.IntegerField()
+    exp_next = models.IntegerField()
 
 class Todo(models.Model):
     title = models.CharField(max_length=30)
